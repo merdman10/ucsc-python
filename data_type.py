f104507939ca6153d8bf2d57cdc535decb36e01a
Monica Erdman <merdman@usgs.gov>
2018-07-17 22:01:21 -0700

    Initial commit.

---
+++ data_type.py
@@ -0,0 +1,8 @@
+#1a Define variables
+a = 15
+b = float(a)
+c = str(a)
+
+#1b Print variables
+a_type = str(type(a))
+print(str(a), "is type:", type(a), \nb, "is type:", type(b), \nc, "is type:", type(c))
