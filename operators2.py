2fab73f1cd6ae78ecf893d01783f6424b05cf2ae
Monica Erdman <merdman@usgs.gov>
2018-07-22 13:04:53 -0700

    Add week 2 operators assignment for Python course

---
+++ operators2.py
@@ -0,0 +1,17 @@
+#Assignment on operators; week 2
+#July 22, 2018
+
+#Problem 1
+A = 100
+if A > 50 and A < 150:
+    print(A)
+else:
+    print("Sorry, the value is out of range.")
+
+#Problem 2
+B = 60
+if B % 5 ==0 or B % 6 ==0:
+    print(B, "is divisible by 5 or 6")
+else:
+    print(B, "is not divisible by 5 or 6")
+
